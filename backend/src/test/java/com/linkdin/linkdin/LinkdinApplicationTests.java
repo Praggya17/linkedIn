10108e5a739515481d58c8239f04e6864970c37d
philonoist17 <praggyapandey02@gmail.com>
2025-12-24 13:15:25 +0530

    chore: set up frontend and backend using spring initializr and vite

---
+++ backend/src/test/java/com/linkdin/linkdin/LinkdinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkdin.linkdin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkdinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
